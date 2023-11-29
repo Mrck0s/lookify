730308ef42300ff48b750b7ada3f5f93eb94374b
Marcos Furco <110942627+Mrck0s@users.noreply.github.com>
2023-11-28 23:09:40 -0300

    Update SongRepository.java

--- src/main/java/com/marcos/lookify/repositories/SongRepository.java
+++ src/main/java/com/marcos/lookify/repositories/SongRepository.java
@@ -4,6 +4,7 @@ import com.marcos.lookify.models.Song;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,5 +12,8 @@ public interface SongRepository extends JpaRepository<Song, Long> {
     Optional<Song> findByTitle(String title);
 
     void deleteById(Long id);
+    Iterable<Song> findByArtistContainingIgnoreCase(String artist);
+
+    List<Song> findTop10ByOrderByRatingDesc();
 
 }
